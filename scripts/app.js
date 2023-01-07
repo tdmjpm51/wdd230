dbd823976d1784bf477ea19cff78ef92407fbaaa
tdmjpm51 <tdmjpm@byui.edu>
2023-01-07 00:56:00 -0700

    update

---
+++ scripts/app.js
@@ -0,0 +1,4 @@
+const h1 = document.querySelector('h1');
+const copyright = document.querySelector("#copyright");
+h1.textContent = 'Sister Janalee McBride';
+copyright.textContent = '© ${new Date().getFullYear()}';
